fdfb8ee4e2ce85445404e6f3e22022cb523e416e
김용준 <rladydwns129@naver.com>
2023-08-24 21:57:14 +0900

    chore: List 구현 setup

---
+++ src/List/DoublyLinkedList.java
@@ -0,0 +1,58 @@
+package List;
+
+public class DoublyLinkedList implements List {
+    @Override
+    public boolean add(int value) {
+        return false;
+    }
+
+    @Override
+    public void add(int index, int value) {
+
+    }
+
+    @Override
+    public int remove(int index) {
+        return 0;
+    }
+
+    @Override
+    public boolean remove(Object value) {
+        return false;
+    }
+
+    @Override
+    public int get(int index) {
+        return 0;
+    }
+
+    @Override
+    public void set(int index, int value) {
+
+    }
+
+    @Override
+    public boolean contains(int value) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(int value) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
