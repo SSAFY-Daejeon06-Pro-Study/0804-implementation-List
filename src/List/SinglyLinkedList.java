278241169352263f7d50f404025d2940963ca7a3
KimYongJun12 <97210866+KimYongJun12@users.noreply.github.com>
2023-08-26 18:58:39 +0900

    fix: 잘못된 메서드 매개변수 수정
    remove(Object value) -> remove(Integer value)

--- src/List/SinglyLinkedList.java
+++ src/List/SinglyLinkedList.java
@@ -17,7 +17,7 @@ public class SinglyLinkedList implements List {
     }
 
     @Override
-    public boolean remove(Object value) {
+    public boolean remove(Integer value) {
         return false;
     }
 
